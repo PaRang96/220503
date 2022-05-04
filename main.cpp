3233eec129fb3b0cc07838ffd85e85ef442ef37f
PaRang96 <winterresie94@gmail.com>
2022-05-04 12:55:29 +0900

    split files practice

--- main.cpp
+++ main.cpp
@@ -1,154 +1,24 @@
-#include <iostream>
-using namespace std;
-
-#define MAX 52
+#include "Common.h"
+#include "initialize.h"
+#include "shuffle.h"
+#include "cardvalue.h"
+#include "draw.h"
+#include "playeradd.h"
+#include "aiadd.h"
 
 int main()
 {
-	int A[MAX] = { 0, };
-	srand((unsigned int)time(0));
+	initialize();
 
-	// initialize
-	for (int i = 0; i < MAX; ++i)
-	{
-		A[i] = i + 1;
-	}
+	shuffle();
+	
+	cardvalue();
 
+	draw();
 
-	// shuffle four times
-	for (int i = 0; i < MAX * 4; ++i)
-	{
-		int FirstNumber = rand() % MAX;
-		int SecondNumber = rand() % MAX;
+	playeradd();
 
-		//Swap
-		int Temp = A[FirstNumber];
-		A[FirstNumber] = A[SecondNumber];
-		A[SecondNumber] = Temp;
-	}
-
-	// 카드 종류
-	string sign;
-	string number;
-
-	// player's 3 cards
-	int Player[3] = {};
-
-	// ai's 3 cards
-	int AI[3] = {};
-
-	// actual cards
-	for (int i = 0; i < MAX; ++i)
-	{
-		switch (A[i] / 13)
-		{
-		case 0:
-			sign = "♥";
-			switch (A[i] % 13)
-			{
-			case 0:
-				number = "A";
-				break;
-			case 10:
-				number = "J";
-				break;
-			case 11:
-				number = "Q";
-				break;
-			case 12:
-				number = "K";
-				break;
-			default:
-				number = (A[i] % 13);
-				break;
-			}
-			break;
-		case 1:
-			sign = "♠";
-			switch (A[i] % 13)
-			{
-			case 0:
-				number = "A";
-				break;
-			case 10:
-				number = "J";
-				break;
-			case 11:
-				number = "Q";
-				break;
-			case 12:
-				number = "K";
-				break;
-			default:
-				number = (A[i] % 13);
-				break;
-			}
-			break;
-		case 2:
-			sign = "♣";
-			switch (A[i] % 13)
-			{
-			case 0:
-				number = "A";
-				break;
-			case 10:
-				number = "J";
-				break;
-			case 11:
-				number = "Q";
-				break;
-			case 12:
-				number = "K";
-				break;
-			default:
-				number = (A[i] % 13);
-				break;
-			}
-			break;
-		case 3:
-			sign = "◈";
-			switch (A[i] % 13)
-			{
-			case 0:
-				number = "A";
-				break;
-			case 10:
-				number = "J";
-				break;
-			case 11:
-				number = "Q";
-				break;
-			case 12:
-				number = "K";
-				break;
-			default:
-				number = (A[i] % 13);
-				break;
-			}
-			break;
-		}
-	}
-
-	// player and ai gets 3 cards randomly
-	for (int i = 0; i < 3; i++)
-	{
-		Player[i] = A[rand() % MAX];
-		AI[i] = A[rand() % MAX];
-	}
-
-	// add player's 3 cards' value
-	int PlayerSum = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		PlayerSum += Player[i] % 13;
-	}
-
-	// add ai's 3 cards' value
-	int AISum = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		AISum += AI[i] % 13;
-	}
+	aiadd();
 
 	// winner decision
 	if (PlayerSum > 21)
